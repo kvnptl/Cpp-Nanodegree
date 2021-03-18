dcc7161a72d49a23324423c3034456aa244565e8
kvnptl <kevinpatel4400@gmail.com>
2021-03-18 17:05:14 +0530

    test

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -4,7 +4,7 @@
 #include <numeric>
 
 using std::vector;
-
+using std::cout;
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
     // Convert inputs to percentage:
