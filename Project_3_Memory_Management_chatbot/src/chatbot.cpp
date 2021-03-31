a7af29a19823ae628b206d631c83a18a96408b04
kvnptl <kevinpatel4400@gmail.com>
2021-03-31 18:59:43 +0530

    task0

--- Project_3_Memory_Management_chatbot/src/chatbot.cpp
+++ Project_3_Memory_Management_chatbot/src/chatbot.cpp
@@ -37,9 +37,9 @@ ChatBot::~ChatBot()
     // deallocate heap memory
     if (_image != NULL) // Attention: wxWidgets used NULL and not nullptr
     {
-        _image = NULL;
         delete _image;
-        }
+        _image = NULL;
+    }
 }
 
 //// STUDENT CODE
