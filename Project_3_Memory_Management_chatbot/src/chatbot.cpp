64b51ad7daccafdcf204f35331d4f9780d7e20c1
kvnptl <kevinpatel4400@gmail.com>
2021-03-31 17:42:54 +0530

    update: task0

--- Project_3_Memory_Management_chatbot/src/chatbot.cpp
+++ Project_3_Memory_Management_chatbot/src/chatbot.cpp
@@ -21,7 +21,7 @@ ChatBot::ChatBot()
 ChatBot::ChatBot(std::string filename)
 {
     std::cout << "ChatBot Constructor" << std::endl;
-    
+
     // invalidate data handles
     _chatLogic = nullptr;
     _rootNode = nullptr;
@@ -35,10 +35,10 @@ ChatBot::~ChatBot()
     std::cout << "ChatBot Destructor" << std::endl;
 
     // deallocate heap memory
-    if(_image != NULL) // Attention: wxWidgets used NULL and not nullptr
+    if (_image != NULL) // Attention: wxWidgets used NULL and not nullptr
     {
         delete _image;
-        _image = NULL;
+        // _image = NULL;
     }
 }
 
