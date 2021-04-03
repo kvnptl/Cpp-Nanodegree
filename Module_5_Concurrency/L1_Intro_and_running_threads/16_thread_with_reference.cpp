dc7a69451a8c86496b4be383a3050b04f4b08ea5
kvnptl <kevinpatel4400@gmail.com>
2021-04-03 17:43:01 +0530

    new: Module 5 Lesson 1 and Lesson 2

---
+++ Module_5_Concurrency/L1_Intro_and_running_threads/16_thread_with_reference.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <thread>
+#include <string>
+
+void printName(std::string &name, int waitTime)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(waitTime));
+    name += " (from Thread)";
+    std::cout << name << std::endl;
+}
+
+int main()
+{
+    std::string name("MyThread");
+
+    // starting thread
+    std::thread t(printName, std::ref(name), 50);
+
+    // wait for thread before returning
+    t.join();
+
+    // print name from main
+    name += " (from Main)";
+    std::cout << name << std::endl;
+
+    return 0;
+}
